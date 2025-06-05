6d51b46cb73f19db0048ae4da955692da2cf0ed3
sirius <916108538@qq.com>
2025-06-05 15:30:33 +0800

    update

--- spring-cloud-gateway-v3-demo/src/main/java/com/hzw/learn/springcloudgatewayv3/filter/HzwGlobleFilter.java
+++ spring-cloud-gateway-v3-demo/src/main/java/com/hzw/learn/springcloudgatewayv3/filter/HzwGlobleFilter.java
@@ -8,17 +8,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.RouteToRequestUrlFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
 @Component
-public class HzwGlobleFilter implements GlobalFilter {
+public class HzwGlobleFilter implements GlobalFilter, Ordered{
     Logger log = LoggerFactory.getLogger(HzwGlobleFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        // 遍历打印headers
+        for (String key : headers.keySet()) {
+            String value = headers.getFirst(key);
+            log.info("===>key:{},value:{}",key,value);
+        }
 
         URI uri = exchange.getRequiredAttribute(GATEWAY_REQUEST_URL_ATTR);
         String uristr = uri.toString();
@@ -28,5 +38,11 @@ public class HzwGlobleFilter implements GlobalFilter {
 
         return chain.filter(exchange);
     }
+
+	@Override
+	public int getOrder() {
+		// return Ordered.HIGHEST_PRECEDENCE + 900;
+        return RouteToRequestUrlFilter.ROUTE_TO_URL_FILTER_ORDER + 1;
+	}
     
 }
